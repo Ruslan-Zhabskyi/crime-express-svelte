02fc70b57d05edfd70bdad8416552cc163348c54
ruslanZhabskyi <ru.zhabskyi@gmail.com>
2024-05-18 16:21:39 +0100

    added upload image to the event

--- src/routes/event/[id]/+page.svelte
+++ src/routes/event/[id]/+page.svelte
@@ -7,6 +7,11 @@
 	import LeafletMap from "$lib/ui/LeafletMap.svelte";
 	import Card from '$lib/ui/Card.svelte';
 	import { generateByCategory } from '$lib/services/report-utils';
+	import { CldUploadWidget } from 'svelte-cloudinary';
+
+	let info;
+	let error;
+	let imageURL;
 
 	export let data;
 	let report: Report = null;
@@ -43,8 +48,8 @@
 	}
   let cat1 = "";
 	onMount(async () => {
-
 		report = await reportService.getReportById(data.data.id, get(currentSession));
+		imageURL = "";
 		cat1 = await getCategoryNameById(report.category);
 		subTitle.set(report.reportName);
 		const weatherCondition = matchWeatherCode(report.code);
@@ -54,7 +59,20 @@
 		mapSatellite.addMarker(report.lat, report.lng, popup, cat1);
 		mapSatellite.moveTo(report.lat, report.lng);
 	});
-
+	async function onUpload(result, widget) {
+		if (result.event === 'success') {
+			info = result.info;
+			imageURL = info.secure_url;
+			// Call the uploadImage method from the report service
+			const updatedReport = await reportService.uploadImage(data.data.id, imageURL, get(currentSession));
+			if (updatedReport) {
+				imageURL = updatedReport.imageURL;
+			}
+		} else if (result.event === 'error') {
+			error = result.error;
+		}
+		widget.close();
+	}
 </script>
 
 
@@ -62,18 +80,23 @@
 <div class="columns">
 	<div class="column">
 		<h1> Report name: {report?.reportName}</h1>
-		<h1> Report image URL: {report?.imageURL}</h1>
 		<h1> Event category: {cat1} </h1>
 		<p> Description: {report?.description}</p>
 		<p> Location: ({report?.lat}, {report?.lng})</p>
 		<p> Timestamp: {report?.timestamp}</p>
 
+		{#if error}
+			<p>Error: {error.status}</p>
+		{/if}
 		{#if report?.imageURL}
 			<figure class="image is-4by3">
 				<img class="object-fit" src={report?.imageURL} alt="crime express logo" />
 			</figure>
 		{/if}
 
+		<CldUploadWidget uploadPreset="cyy29psm" let:open let:isLoading {onUpload}>
+			<button on:click={open} disabled={isLoading} class="button is-success is-fullwidth">Upload Image</button>
+		</CldUploadWidget>
 	</div>
 
 <div class="column">
